baec1d228b061a58fd3ce87bee313ec025a9cfa5
cjf521 <cjf521@york.ac.uk>
2014-02-13 21:09:02 +0000

    SplashState loading bar pre-implementation
    !non-functional loading bar added

--- Game/src/states/SplashScreen.java
+++ Game/src/states/SplashScreen.java
@@ -1,7 +1,9 @@
 package states;
 
+import java.awt.Rectangle;
 import java.io.IOException;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -15,9 +17,7 @@ import org.newdawn.slick.state.StateBasedGame;
 public class SplashScreen extends BasicGameState {
 
 	private Image splash;
-	
-	private int elapsedTime;
-	private final int DELAY = 6000;
+	private LoadingList loading = LoadingList.get(); 
 	
 	public SplashScreen(int state){
 		
@@ -32,29 +32,44 @@ public class SplashScreen extends BasicGameState {
 	@Override
 	public void render(GameContainer gc, StateBasedGame s, Graphics g)
 			throws SlickException {
+		
 		g.drawImage(splash, 0, 0);
+		
+		int	ox = stateContainer.Game.MAXIMUMWIDTH /4,
+			oy = stateContainer.Game.MAXIMUMHEIGHT /16;
+		Rectangle 
+			loadBase = new Rectangle(ox, 10*oy, 2*ox, oy),
+			loadFill = new Rectangle(loadBase);	
+		loadFill.width = (loadFill.width * loading.getRemainingResources()) 
+				/ loading.getTotalResources();
+		loadFill.grow(-4, -4);
+		
+		g.setColor(Color.white);
+		g.fillRoundRect(loadBase.x, loadBase.y, 
+				loadBase.width, loadBase.height, oy/3);
+		g.setColor(Color.black);
+		g.fillRoundRect(loadFill.x, loadFill.y, 
+				loadFill.width, loadFill.height, oy/3);	
 	}
+	
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame s, int delta)
 			throws SlickException {
 		int remainingResources = LoadingList.get().getRemainingResources();
+		
 		while( remainingResources > 0){
-				
-			 DeferredResource nextResource = LoadingList.get().getNext();
-			 try {
+
+			DeferredResource nextResource = loading.getNext();
+			try {
 				nextResource.load();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			 remainingResources = LoadingList.get().getRemainingResources();
+			remainingResources = loading.getRemainingResources();
 		}
-		//elapsedTime += delta;
-		
-		//if(elapsedTime >= DELAY){
-			s.enterState(1);
-		//}
+
+		s.enterState(stateContainer.Game.MENUSTATE);
 	}
 
 	@Override
