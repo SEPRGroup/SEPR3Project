fea8d973c1bd0b99949d24d45ae52d25de87a25f
cjf521 <cjf521@york.ac.uk>
2014-02-19 19:15:24 +0000

    SplashScreen updated
    -included .jpg compressed version of startup_bg to decrease loading
    times
    
    SplashScreen:
    -now pretty!

--- Game/src/states/SplashState.java
+++ Game/src/states/SplashState.java
@@ -1,9 +1,9 @@
 package states;
 
-import java.awt.Rectangle;
+//import java.awt.Rectangle;
 import java.io.IOException;
 
-import org.newdawn.slick.Color;
+//import org.newdawn.slick.Color;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -16,9 +16,10 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public final class SplashState extends BasicGameState {
 
-	private static Image splash;
-	private static Color
-		loadBaseColor = Color.white, loadFillColor = Color.black;
+	private static Image 
+		splash, indicator;
+	/*private static Color
+		loadBaseColor = Color.white, loadFillColor = Color.black;*/
 	private static LoadingList loading = LoadingList.get();
 	
 	
@@ -30,7 +31,8 @@ public final class SplashState extends BasicGameState {
 	@Override
 	public void init(GameContainer gc, StateBasedGame s)
 			throws SlickException {
-		splash = new Image("res/graphics/startup_bg.png");
+		splash = new Image("res/graphics/new/startup_bg.jpg");
+		indicator = new Image("res/graphics/new/startup_plane.png");
 	}
 
 	@Override
@@ -38,10 +40,13 @@ public final class SplashState extends BasicGameState {
 			throws SlickException {
 		
 		g.drawImage(splash, 0, 0);
+		indicator.drawCentered(900 -((600 * loading.getRemainingResources()) 
+				/ loading.getTotalResources()), 390);
 		
-		//calculate loading bar sizes
+		/*//calculate loading bar sizes
 		int	ox = stateContainer.Game.MAXIMUMWIDTH /4,	// offset/sizing values
 			oy = stateContainer.Game.MAXIMUMHEIGHT /16;
+		
 		Rectangle 
 			loadBase = new Rectangle(ox, 10*oy, 2*ox, oy),
 			loadFill = new Rectangle(loadBase);	
@@ -56,7 +61,8 @@ public final class SplashState extends BasicGameState {
 				loadBase.width, loadBase.height, oy/3);
 		g.setColor(loadFillColor);
 		g.fillRoundRect(loadFill.x, loadFill.y, 
-				loadFill.width, loadFill.height, oy/3 -1);	
+				loadFill.width, loadFill.height, oy/3 -1);*/
+		
 	}
 	
 	@Override
